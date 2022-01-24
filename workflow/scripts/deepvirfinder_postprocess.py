d39d2745becf0831300f9cbf639539ed4ba74e89
Remi Denise <remi.denise@gmail.com>
2022-01-24 23:10:22 +0000

    :bug: Wrong name for deepvirfinder_postprocess.py output in script

--- workflow/scripts/deepvirfinder_postprocess.py
+++ workflow/scripts/deepvirfinder_postprocess.py
@@ -22,7 +22,7 @@ deepvirfinder_file_df = pd.read_table(deepvirfinder_file)
 # Good hit virfinder (form Camarillo-Gurerro_2021_Cell): score > 0.9 and pvalue < 0.01
 deepvirfinder_file_df[(deepvirfinder_file_df.score > 0.9) &
                       (deepvirfinder_file_df.pvalue < 0.01)
-                      ].name.to_csv(output.snakemake.txt, index=False)
+                      ].name.to_csv(snakemake.output.txt, index=False)
 
 ###########################################################
 ###########################################################
