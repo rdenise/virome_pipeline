bdd5c25e66e4b78ada30eab4269019577e9a04d8
rdenise <remi.denise@gmail.com>
2022-01-25 15:10:04 +0000

    :sparkles: new adding in combine virsorter and virfinder back

--- workflow/scripts/combine_virsorter_virfinder.py
+++ workflow/scripts/combine_virsorter_virfinder.py
@@ -1,4 +1,5 @@
 from Bio import SeqIO
+import pandas as pd
 import sys
 import os
 
@@ -11,6 +12,9 @@ sys.stderr = sys.stdout = open(snakemake.log[0], "w")
 # List that will contains all the contigs to filter
 all_contig_ids = []
 
+# Dataframe that contains all the informations about
+output_df = pd.DataFrame(columns=['contig_id', 'virsorter_cat', 'deepvirfinder'])
+
 # Get all the names from the virsorter keep2 list
 ids_virsorter_keep2 = snakemake.input.ids_virsorter_keep2
 
@@ -18,7 +22,12 @@ with open(ids_virsorter_keep2) as r_file:
     r_file.readline()
 
     for line in r_file:
-        all_contig_ids.append(line.rsplit())
+        rstrip_line = line.rstrip()
+
+        all_contig_ids.append(rstrip_line)
+
+        output_df.at[rstrip_line, "contig_id"] = rstrip_line
+        output_df.at[rstrip_line, "virsorter_cat"] = "keep2"
 
 # Get all the names from the virsorter keep1 list and remove redondant name
 ids_virsorter_keep1 = snakemake.input.ids_virsorter_keep1
@@ -27,10 +36,13 @@ with open(ids_virsorter_keep1) as r_file:
     r_file.readline()
 
     for line in r_file:
-        rsplit_line = line.rsplit()
+        rstrip_line = line.rstrip()
         
-        if rsplit_line not in all_contig_ids:
-            all_contig_ids.append(rsplit_line)
+        if rstrip_line not in all_contig_ids:
+            all_contig_ids.append(rstrip_line)
+
+            output_df.at[rstrip_line, "contig_id"] = rstrip_line
+            output_df.at[rstrip_line, "virsorter_cat"] = "keep1"
 
 # Get all the names from the deepvirfinder list and remove redondant name
 ids_virfinder = snakemake.input.ids_virfinder
@@ -39,10 +51,13 @@ with open(ids_virfinder) as r_file:
     r_file.readline()
 
     for line in r_file:
-        rsplit_line = line.rsplit()
-        
-        if rsplit_line not in all_contig_ids:
-            all_contig_ids.append(rsplit_line)
+        rstrip_line = line.rstrip()
+
+        output_df.at[rstrip_line, "contig_id"] = rstrip_line
+        output_df.at[rstrip_line, "deepvirfinder"] = "Yes"
+
+        if rstrip_line not in all_contig_ids:
+            all_contig_ids.append(rstrip_line)
 
 # Parse the fasta of the contig and create the new one
 fasta_contigs = snakemake.input.contigs
@@ -59,7 +74,7 @@ with open(snakemake.output.fasta, "w") as w_file:
 
             SeqIO.write(contig, w_file, "fasta")
             
-
+output_df.to_csv(snakemake.output.tsv, sep="\t", index=False)
 
 ###########################################################
 ###########################################################
