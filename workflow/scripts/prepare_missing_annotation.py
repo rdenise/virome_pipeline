1c5c05c52d78b50e410ea733ee1eb3e323e39498
rdenise <remi.denise@gmail.com>
2022-05-06 12:52:35 +0100

    :bug: wrong script and name for missing annotation

--- workflow/scripts/prepare_missing_annotation.py
+++ workflow/scripts/prepare_missing_annotation.py
@@ -11,12 +11,15 @@ sys.stderr = sys.stdout = open(snakemake.log[0], "w")
 tsv_df = pd.read_table(snakemake.input.tsv)
 
 all_wanted = tsv_df[(tsv_df.virsorter_cat.isna()) | ~(tsv_df.virsorter_cat.str.contains('keep'))]
-all_wanted = tsv_df.contig_id.tolist()
+all_wanted = all_wanted.contig_id.tolist()
 
 with open(snakemake.output.fasta, 'wt') as w_file:
     parser = SeqIO.parse(snakemake.input.fasta, 'fasta')
 
     for contig in parser:
+        # Because name in selected tsv and fasta file are different, the fasta file have {name_contig}-contigs- at the begining
+        contig_id = contig.id.split('contigs-')[-1]
+
         if contig.id in all_wanted:
             SeqIO.write(contig, w_file, "fasta")
 
